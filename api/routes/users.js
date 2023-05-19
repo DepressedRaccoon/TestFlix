62e4bec74ce5555d629cf1654342b1c8511a2444
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-05-19 01:15:34 -0500

    Added token verification and update user route.

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,6 +6,26 @@ const CryptoJS = require('crypto-js');
 
 // Update User
 
+router.put('/:id', async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set:req.body
+            });
+            res.status(200).json(updatedUser);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You can onlay update your account!");
+    }
+
+
+
+});
 // Delete User
 
 // GET user
