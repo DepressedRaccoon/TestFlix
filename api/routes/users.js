23d47dc39559313886bbfc0ea812269f6b505017
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-05-19 01:39:59 -0500

    Added Delete, Get, and Get All to userRoutes

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const CryptoJS = require('crypto-js');
 
 const verify = require('../verifyToken');
+const { query } = require('express');
 
 // Update User
 
@@ -46,7 +47,36 @@ router.delete('/:id', verify, async (req, res) => {
 
 // GET user
 
+router.get('/find/:id',  async (req, res) => {
+        try {
+           const user = await User.findById(req.params.id);
+           const { password, ...info } = user._doc;
+            res.status(200).json(info);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    } 
+);
+
 // GET all users
+
+router.get('/', verify, async (req, res) => {
+
+    const query = req.query.new;
+
+    if (req.user.isAdmin) {
+        try {
+           const users = query ? await User.find().limit(10) : await User.find(); 
+            res.status(200).json(users);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("Hey you're not allowed to be here!");
+    }
+});
 
 // GET user stats
 
