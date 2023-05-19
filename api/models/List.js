3b237db315b4ba8f54e45ff434988c02d644e4b3
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-05-19 00:22:16 -0500

    Created models
    Added Listings for movies, Movie, and user models

---
+++ api/models/List.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ListSchema = new mongoose.Schema(
+    {
+
+        title: { type: String, required: true, unique: true },
+        type: { type: String, required: true },
+        genre: { type: String },
+        content: { type: Array},
+
+    },
+
+    { timestamps: true }
+
+);
+
+module.export = mongoose.model("List", ListSchema); 
