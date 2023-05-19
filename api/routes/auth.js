6fbefa9ac9662483e66bfbb6739f362c51f35324
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-05-19 01:02:09 -0500

    Add JWT authentication
    Added JSON Web Token Authentication with an expiration date of 5 Days.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,6 +4,8 @@ const User = require('../models/User');
 
 const CryptoJS = require('crypto-js');
 
+const jwt = require("jsonwebtoken");
+
 // User Regitsration
 
 router.post('/register', async (req, res) => { 
@@ -44,9 +46,12 @@ router.post('/register', async (req, res) => {
 
             originalPassword !== req.body.password && res.status(401).json('Wrong username or password!');
 
+            const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.SECRET_KEY,{expiresIn:"5d"}
+            ); 
+
             const { password, ...info } = user._doc;
 
-           res.status(200).json(user);  
+           res.status(200).json({...info, accessToken});  
 
         } catch (err) {
             res.status(500).json(err);
---
+++ api/routes/users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const User = require('../models/User');
+
+const CryptoJS = require('crypto-js');
+
+// Update User
+
+// Delete User
+
+// GET user
+
+// GET all users
+
+// GET user stats
+
