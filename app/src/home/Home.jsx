70b4227c076e50decde7c15ca7ee06d024233285
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-20 22:59:30 -0500

    allow homepage to render without errors

--- app/src/home/Home.jsx
+++ app/src/home/Home.jsx
@@ -1,18 +1,17 @@
-import List from "../../../api/models/List"
-import Navbar from "../components/topbar/Navbar"
+// import List from "../../../api/models/List"
+// import Featured from "../components/featured/Featured"
+// import List from "../components/list/List"
 import "./home.scss"
-import Featured from "../components/featured/Featured"
-import List from "../components/list/List"
 
 const home = ({type}) => {
   return (
     <div className="home">
-        <Navbar/> 
-        <Featured type={type}/>
+        HOME PAGE
+        {/* <Featured type={type}/>
         <List/>
         <List/>
         <List/>
-        <List/>
+        <List/> */}
     </div>
   )
 }
