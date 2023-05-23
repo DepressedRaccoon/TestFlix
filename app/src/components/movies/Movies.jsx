b675f3be3ad202bb6d300b417bcc2d23b879ae22
conrad <conradkg1@gmail.com>
2023-05-22 20:50:32 -0500

    movies

--- app/src/components/movies/Movies.jsx
+++ app/src/components/movies/Movies.jsx
@@ -5,6 +5,10 @@ const Movies = () => {
     return(
         <div className="movies">
             MOVIE PAGE
+            <img class='movieImg' src='app\public\images\Adventure To South America.png'></img>
+            <img class='movieImg' src='app\public\images\Just Dance Revolution.png'></img>
+            <img class='movieImg' src='app\public\images\Talking Frog Convinces Son to Kill Father 2.png'></img>
+            <img class='movieImg' src='app\public\images\Western Time Travelers.png'></img>
         </div>
     )
 }
