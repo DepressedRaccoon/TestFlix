9c5eb7275ad3f254c4dbb9915c98b909b9d52b7e
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-05-24 16:58:48 -0500

    Add Login and Context functions for JWT sign-in

--- app/src/components/login/Login.jsx
+++ app/src/components/login/Login.jsx
@@ -1,11 +1,33 @@
-import "./Login.scss"
+import React, { useState } from "react";
+import "./Login.scss";
 
-const Login = () => {
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="login">
-      LOGIN PAGE
+      <form className="loginForm">
+        <input
+          type="text"
+          placeholder="email"
+          className="loginInput"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="loginInput"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="loginButton" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
     </div>
-  )
+  );
 }
-
-export default Login;
