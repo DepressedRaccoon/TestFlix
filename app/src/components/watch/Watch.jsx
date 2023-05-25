d6f62eedecbdcf53c614209f37aabd7878dc2423
conrad <conradkg1@gmail.com>
2023-05-24 19:02:24 -0500

    watchpage

---
+++ app/src/components/watch/Watch.jsx
@@ -0,0 +1,12 @@
+import "./topbar/Navbar";
+import "./Watch.scss";
+
+const Watch = () => {
+    return(
+        <div className="watch">
+           
+        </div>
+    )
+}
+
+export default Series;
